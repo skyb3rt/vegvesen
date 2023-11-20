f3beac5fe9cbbed12a2b9ed8e0e4608164ef2cf4
Thomas Kristoffersen <t.kristoffersen@gmail.com>
2023-11-21 00:34:57 +0100

    readme, output

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# VEGVESEN
+## VEGVESEN
 Enkel app for å hente kjøretøyinformasjon fra vegvesen.no
 
 
-## avhengigheter:
-api key må fylles ut i .env filen
+### Avhengigheter:
+Api nøkkel hentes ut fra Din Side på vegvesen.no og fylles ut i .env filen.
+
+### Henting av kjøretøyinformasjon:
+pip install -r requirements.txt
+
+Kjennemerke fylles ut i app/main.py
+
+python app/main.py
+
+kjøretøyinformasjon blir lagret som json fil i output katalogen
+
+### eksempel på utskrift:
+kjennemerke='DP99999', merke='FORD', modell='MONDEO', kw=92.0, kontrollfrist='2025-04-30'
--- app/main.py
+++ app/main.py
@@ -9,16 +9,21 @@ API_KEY=os.environ.get("VEGVESEN_API_KEY")
 VEGVESEN_URL=os.environ.get("VEGVESEN_URL")
 
 kjennemerke="DP24034"
-session = requests.Session()
-session.params.update({'kjennemerke':kjennemerke})
-session.headers.update({'SVV-Authorization': f"Apikey {API_KEY}"})
-response=session.get(VEGVESEN_URL)
-kjoretoy_json=response.json()
-kjoretoy=Model(**kjoretoy_json).kjoretoydataListe[0]
-kjennemerke=kjoretoy.kjoretoyId.kjennemerke
-kontrollfrist=kjoretoy.periodiskKjoretoyKontroll.kontrollfrist
-merke=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.generelt.merke[0].merke
-modell=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.generelt.handelsbetegnelse[0]
-kw=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.motorOgDrivverk.motor[0].drivstoff[0].maksNettoEffekt
-print(F"{kjennemerke=},{merke=},{modell=},{kw=},{kontrollfrist=}")
 
+def main():
+    session = requests.Session()
+    session.params.update({'kjennemerke':kjennemerke})
+    session.headers.update({'SVV-Authorization': f"Apikey {API_KEY}"})
+    response=session.get(VEGVESEN_URL)
+    kjoretoy_json=response.json()
+    with open(f"output/{kjennemerke}.json","w") as f:
+        f.write(json.dumps(kjoretoy_json, indent=4,ensure_ascii=False))
+    kjoretoy=Model(**kjoretoy_json).kjoretoydataListe[0]
+    kontrollfrist=kjoretoy.periodiskKjoretoyKontroll.kontrollfrist
+    merke=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.generelt.merke[0].merke
+    modell=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.generelt.handelsbetegnelse[0]
+    kw=kjoretoy.godkjenning.tekniskGodkjenning.tekniskeData.motorOgDrivverk.motor[0].drivstoff[0].maksNettoEffekt
+    print(F"{kjennemerke=}, {merke=}, {modell=}, {kw=}, {kontrollfrist=}")
+
+if __name__ == "__main__":
+    main()
